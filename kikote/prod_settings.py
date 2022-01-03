5f94a223093e49654126cd5526a4dea5e1d0ee4f
maxdev01 <maximemilien01@gmail.com>
2022-01-03 23:54:32 +0100

    configuration databases postgreesql

--- kikote/prod_settings.py
+++ kikote/prod_settings.py
@@ -1,6 +1,8 @@
+import dj_database_url
+
 from .settings import *
 
-
+DATABASES['default'] = dj_database_url.config()
 DEBUG = False
 
 SECRET_KEY = 'jw8z072=h=&e=tlr4@5ddh^x4l4i$u54ezs(!#=9u0(e+$ma%j'
