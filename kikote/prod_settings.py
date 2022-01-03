b3a2f9b6e1d6214503e7d1ea3a835762e116b01f
maxdev01 <maximemilien01@gmail.com>
2022-01-03 23:36:29 +0100

    prod_settings heroku

--- kikote/prod_settings.py
+++ kikote/prod_settings.py
@@ -1,8 +1,10 @@
-from settings import *
+from .settings import *
 
 
 DEBUG = False
 
+SECRET_KEY = 'jw8z072=h=&e=tlr4@5ddh^x4l4i$u54ezs(!#=9u0(e+$ma%j'
+
 TEMPLATE_DEBUG = False
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = [''] 
